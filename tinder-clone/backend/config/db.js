4518bbe199eb7a93af99c5766c0458439b3a0767
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-12 11:51:22 +0000

    setup database connection use connection using mongoose

---
+++ tinder-clone/backend/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
+    mongoose.set('debug', true);
+
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.log('cant connect to database');
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
