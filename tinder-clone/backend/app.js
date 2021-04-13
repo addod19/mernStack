6987a08824fb3ea8f5def3a530180768accc3cf3
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-13 14:26:05 +0000

    fix errors with named import

--- tinder-clone/backend/app.js
+++ tinder-clone/backend/app.js
@@ -1,13 +1,45 @@
-const express = require('express');
-const connectDB = require('./config/db');
+import express from 'express';
+import connectDB from './config/db.js';
 
+import Cards from './dbCards.js';
+
+// App config
 const app = express();
+const port = process.env.PORT || 8082;
 
 // connect database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!!'));
+// Middlewares
 
-const port = process.env.PORT || 8082;
 
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello world!!'));
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = res.body;
+
+    Cards.create(dbCard, (error, data) => {
+        if(error) {
+            req.status(500).send(error);
+        }
+        else {
+            res.status(201).send(`Created data: ${data}`);
+        }
+    })
+})
+
+app.get('/tinder/card', (req, res) => {
+    const dbCard = res.body;
+
+    Cards.find((error, data) => {
+        if(error) {
+            req.status(500).send(error);
+        }
+        else {
+            res.status(200).send(data);
+        }
+    })
+})
+// Listener
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- tinder-clone/backend/config/db.js
+++ tinder-clone/backend/config/db.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+import mongoose from 'mongoose';
+import config from 'config';
 
+
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
@@ -22,4 +23,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
