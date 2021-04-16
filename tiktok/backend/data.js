91567ee1a916f128a40bcffd6c145a667c19312f
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-16 17:39:30 +0000

    add data.js file and improve server function by adding another end point

---
+++ tiktok/backend/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        url: 
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        channel: "nanakwame",
+        description: "My first day outside the womb",
+        song: "singing lullaby",
+        likes: 135,
+        shares: 201,
+        messages: 89
+    },
+    {
+        url: 
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        channel: "nanaAddo",
+        description: "My first professional photo",
+        song: "Possing for the camera",
+        likes: 125,
+        shares: 101,
+        messages: 69
+    }
+]
+
+export default data;
--- tiktok/backend/server.js
+++ tiktok/backend/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
+import Data from './data.js';
 
+// app config
+const app = express();
 const port = 9002;
 
+// middlewares
+
+
+// db config
+
+
+// api endpoints
 app.get('/', (req,res) => res.status(200).send("Hello Daniel Larbi Addo"));
 
+
+app.get('/v1/posts', (req, res) => res.status(200).send(Data));
+
+// listen
 app.listen(port, () => console.log(`listening on localhost:${port}`));
