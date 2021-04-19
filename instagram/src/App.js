1e0852c421114d3c5491d1adf76331ff68deb71b
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-19 14:52:59 +0000

    fix image not appearing

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+import Post from './components/Post';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hey!!! instagram clone is almost up</h1>
+    <div className="app">
+      <img
+      className="app__header__logo"
+      src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" className="App-logo" alt="logo" />
+    
+    <Post />
     </div>
   );
 }
