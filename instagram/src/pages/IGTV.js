80e4cf236df087a2e7c7b54e9062a09e600f9253
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-21 12:51:03 +0000

    style the other pages

--- instagram/src/pages/IGTV.js
+++ instagram/src/pages/IGTV.js
@@ -1,10 +1,60 @@
 import React from 'react';
 
+import '../assets/css/IGTV.css';
+
+import IGTVNgu from '../assets/ngu.png';
+import IGTVBarbar from '../assets/barber.png';
+import IGTVYm from '../assets/ym.png';
+import IGTVPatrik from '../assets/patrik.png';
+import IGTVCoconut from '../assets/coconut.png';
+import IGTVProf from '../assets/prof.png';
+
 
 const IGTV = () => {
   return(
     <section className="igtv">
-     IGTV SECTION
+     <div className="igtv_never__give__up">
+      <img 
+        className="igtv_nvu__img"
+        src={IGTVNgu}
+        alt="post ngu"
+       />
+     </div>
+     <div className="igtv__barbar">
+      <img 
+        className="igtv_barbar__img"
+        src={IGTVBarbar}
+        alt="post barbar"
+       />
+     </div>
+     <div className="igtv_ym">
+      <img 
+        className="igtv_ym__img"
+        src={IGTVYm}
+        alt="post ym"
+      />
+     </div>
+     <div className="igtv_patrik">
+      <img 
+        className="igtv_patrik__img"
+        src={IGTVPatrik}
+        alt="post patrik"
+       />
+     </div>
+     <div className="igtv_coconut">
+     <img 
+        className="igtv_coconut__img"
+        src={IGTVCoconut}
+        alt="post coconut"
+       />
+     </div>
+     <div className="igtv_prof">
+       <img 
+        className="igtv_prof_image"
+        src={IGTVProf}
+        alt="post prof"
+       />
+     </div>
     </section>
   )
 }
--- instagram/src/pages/Saved.js
+++ instagram/src/pages/Saved.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import '../assets/css/Saved.css';
+
+import SavedBarbar from '../assets/barber.png';
+
 
 const Saved = () => {
   return(
     <section className="saved">
-     Saved SECTION
+     <div className="saved__barbar">
+      <img 
+        className="saved_barbar__img"
+        src={SavedBarbar}
+        alt="saved barbar"
+       />
+     </div>
     </section>
   )
 }
