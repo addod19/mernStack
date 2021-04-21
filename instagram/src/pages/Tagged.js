7167f94fba7833bf24a37a2fd39195a6b3410412
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-04-21 10:37:02 +0000

    link tagged.css file and make the footer position fixed

--- instagram/src/pages/Tagged.js
+++ instagram/src/pages/Tagged.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import TaggedNgu from '../assets/ngu.png';
+import TaggedBarbar from '../assets/barber.png';
+
+import '../assets/css/Tagged.css';
 
 const Tagged = () => {
   return(
     <section className="tagged">
-     tagged section
+     <div className="tagged_never__give__up">
+      <img 
+        className="tagged_nvu__img"
+        src={TaggedNgu}
+        alt="tagged ngu"
+       />
+     </div>
+     <div className="tagged__barbar">
+      <img 
+        className="tagged_barbar__img"
+        src={TaggedBarbar}
+        alt="tagged barbar"
+       />
+     </div>
     </section>
   )
 }
